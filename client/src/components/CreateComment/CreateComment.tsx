f393ef172008c194b5f2d9a5cd68b434ef981459
Anton <60780638+antonio63963@users.noreply.github.com>
2022-12-23 18:49:12 +0200

    client: change layout on breakpoint sm

--- client/src/components/CreateComment/CreateComment.tsx
+++ client/src/components/CreateComment/CreateComment.tsx
@@ -28,36 +28,44 @@ const CreateComment: FC<CreateCommentProps> = ({
 }) => {
   return (
     <>
-      <div className="mt-6 flex fex-wrap gap-3">
-        <Link
-          to={`/user-profile/:${user.id}`}
-          className="flex gap-2 items-center bg-white rounded-lg"
-        >
-          {user.picture ? (
-            <img
-              src={user.picture}
-              alt="userImage"
-              className="w-10 rounded-full"
-            />
-          ) : (
-            <NoAvatar theme={"light"} />
-          )}
-        </Link>
-        <textarea
-          className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
-          placeholder="Add your comment"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-      <div className="">
-            <p className={`mb-2 ${comment.length > 300 ? 'text-red-500' : 'text-gray-400'}`}>{comment.length}/300</p>
+      <div className="mt-6 flex fex-wrap gap-3 max-sm:flex-col">
+        <div className="flex items-center w-full">
+          <Link
+            to={`/user-profile/:${user.id}`}
+            className="flex mr-2 items-center bg-white rounded-lg"
+          >
+            {user.picture ? (
+              <img
+                src={user.picture}
+                alt="userImage"
+                className="w-10 rounded-full"
+              />
+            ) : (
+              <NoAvatar theme={"light"} />
+            )}
+          </Link>
+          <textarea
+            className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+            placeholder="Add your comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <p
+            className={`mb-2 ${
+              comment.length > 300 ? "text-red-500" : "text-gray-400"
+            }`}
+          >
+            {comment.length}/300
+          </p>
           <button
             className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
             onClick={addComment}
           >
             {isAddComment ? "Posting the comment..." : "Post"}
           </button>
-      </div>
+        </div>
       </div>
       {errorComment && (
         <p className=" ml-20 my-3 text-red-500 text-sm">{errorComment}</p>
