735257413349a2731b18daa6e32cead8257b5dcc
Anton <60780638+antonio63963@users.noreply.github.com>
2022-12-23 14:12:14 +0200

    server: added multer, save avatar

---
+++ server/src/middlewares/upload.ts
@@ -0,0 +1,49 @@
+import multer from "multer";
+import path from "path";
+import { Request } from "express";
+import { v4 as uuidv4 } from 'uuid';
+
+const folderUploads = path.resolve("upload/avatars/");
+
+const storage = multer.diskStorage({
+  destination(req: Request, file, cb) {
+    cb(null, folderUploads);
+  },
+  filename(req, file, cb) {
+    console.log('Multer', file)
+    const typeFile = file.mimetype.match(/\/(.*)$/i)[1];
+    const filePath = `${uuidv4()}_${Date.now()}.${typeFile}`;
+
+    cb(null, `${filePath}`);
+    req.params.photoPath += `,${filePath}`;
+    console.log(req.params)
+  },
+});
+
+const fileFilter = (req: Request, file: any, cb: any) => {
+  const typeFile = file.mimetype;
+  if (
+    typeFile === "image/png" ||
+    typeFile === "image/jpg" ||
+    typeFile === "image/jpeg" ||
+    typeFile === "image/svg" ||
+    typeFile === "image/gif" ||
+    typeFile === "image/tiff"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const limits = {
+  fileSize: 2e7,
+};
+
+const uploadFile = multer({ storage, fileFilter, limits }).single("imageAsset");
+const parseText = multer().none();
+
+export {
+  uploadFile,
+  parseText,
+};
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,11 +2,12 @@ import express from 'express';
 
 import AuthController from 'controllers/AuthController';
 import checkGoogle from 'middlewares/checkGoogle';
+import {uploadFile} from 'middlewares/upload';
 
 const authRouter = express.Router();
 
 authRouter.post('/signin', AuthController.signin);
-authRouter.post('/signUp', AuthController.singnUp);
+authRouter.post('/signUp', uploadFile, AuthController.singnUp);
 authRouter.post('/googleAuth', checkGoogle, AuthController.googleAuth);
 authRouter.post('/logout', AuthController.logout);
 
