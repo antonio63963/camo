849cdda522a46ea71b5d51b447be1437018068ee
Anton <60780638+antonio63963@users.noreply.github.com>
2022-12-26 11:10:27 +0200

    client: axios service interseptor on 401 refresh

--- client/src/services/axios.service.ts
+++ client/src/services/axios.service.ts
@@ -1,55 +1,78 @@
-import axios, { AxiosResponse, AxiosError } from 'axios';
-import type { createBrowserHistory } from 'history';
-import routes from '../routes';
+import axios, { AxiosResponse, AxiosError, AxiosRequestConfig } from "axios";
+import type { createBrowserHistory } from "history";
+import routes from "../routes";
 
-import jwtService from './storage.service';
+import storageService from "./storage.service";
 
 type Tokens = {
   accessToken: string;
   refreshToken: string;
 };
 
+type TConfig = {
+  [index: string]: any;
+};
+
 class AxiosService {
   init(history: ReturnType<typeof createBrowserHistory>) {
+    // axios.defaults.withCredentials = true;
     axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-    axios.interceptors.response.use((response: AxiosResponse): AxiosResponse<any, any> => {
-      if(response.data?.tokens) {
-        this.setAuthorizationHeader(response.data.tokens);
-      };
-      return response;
-    },
-    (error: AxiosError) => {
-      if (error.response) {
-        console.log('Axios: ', error.response.status, history.location.pathname)
+    axios.interceptors.response.use(
+      (response: AxiosResponse): AxiosResponse<any, any> => {
+        if (response.data?.tokens) {
+          this.setAuthorizationHeader(response.data.tokens);
+        }
+        return response;
+      },
+      async (error: AxiosError) => {
+        const originalRequest = error.config as TConfig;
+        console.log(originalRequest)
+        if (error.response) {
+          if (
+            error.response.status === 401 &&
+            !originalRequest._isRetry
+            ) {
+            try {
+              originalRequest._isRetry = true;
+              const { refreshToken } = storageService.getTokens();
+              const { data: {status, tokens} } = await axios.post<{status: string, tokens: Tokens}>("/auth/refresh", {
+                refreshToken,
+              });
+              console.log(status , tokens)
+              storageService.saveTokens(tokens);
+              this.setAuthorizationHeader(tokens);
+              originalRequest.headers.Authorization = `Bearer ${tokens.accessToken}`;
+              return await axios.request(originalRequest);
+            } catch (err) {
+              history.push(routes.AUTH_SIGNIN);
+            }
+          } else {
+            storageService.destroyTokens();
+            this.setAuthorizationHeader();
+            history.push(routes.AUTH_SIGNIN);
+          }
 
-        if (
-          error.response.status === 401 &&
-          history.location.pathname !== routes.AUTH_SIGNIN
-        ) {
-          jwtService.destroyTokens();
-          this.setAuthorizationHeader();
-          history.push(routes.AUTH_SIGNIN);
+          if (
+            error.response.status === 404
+          ) {
+            history.push(routes.NOT_FOUND);
+          }
         }
-        if (
-          error.response.status === 404 &&
-          error.response.config.method === 'get'
-        ) {
-          history.push(routes.NOT_FOUND);
-        }
+        return Promise.reject(error);
       }
-      return Promise.reject(error);
-    });
+    );
 
-    this.setAuthorizationHeader(jwtService.getTokens());
+    this.setAuthorizationHeader(storageService.getTokens());
   }
 
   setAuthorizationHeader(tokens?: Tokens | null) {
     if (tokens) {
-      axios.defaults.headers.common.Authorization = `Bearer ${JSON.stringify(tokens)}`;
+      axios.defaults.headers.common.Authorization = `Bearer ${tokens.accessToken}`;
+      console.log("HEADER: ", axios.defaults.headers.common.Authorization)
     } else {
       delete axios.defaults.headers.common.Authorization;
     }
   }
-};
+}
 
 export default new AxiosService();
